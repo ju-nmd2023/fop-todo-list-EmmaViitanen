4b330a01cfa217acbc5c1a69a7e2b20e52251861
Emma Viitanen <viem23ni@student.ju.se>
2024-03-29 12:47:16 +0100

    Centered the list on the page

--- toDo.js
+++ toDo.js
@@ -1,4 +1,28 @@
-const toDoListElement = document.getElementById(toDoList);
-console.log(toDoListElement);
+// let tasks = [];
+// const toDoListElement = document.getElementById("toDoList");
+// console.log(toDoListElement);
 
-let list = [];
+// let list = [];
+
+// for (let task in tasks) {
+//   const taskElement = document.createElement("div");
+//   taskElement.innerText = task;
+
+//   taskElement.classList.add("task");
+//   taskElement.onclick = addToToDoList;
+//   toDoListElement.appendChild(taskElement);
+// }
+
+// function addToToDoList() {
+//   list.push(this.innerText);
+
+//   const theTask = document.createElement("div");
+
+//   const taskSpan = document.createElement("span");
+//   taskSpan.innerText = this.innerText;
+//   theTask.appendChild(taskSpan);
+
+//   const checkButton = document.createElement("button");
+//   button.innerText = "Done";
+//   button.onclick = theTask.style.bold;
+// }
