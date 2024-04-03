de4f6f2b17e34bfac910154426e177d0beaf62aa
Emma Viitanen <viem23ni@student.ju.se>
2024-04-03 11:35:42 +0200

    Updated js code

--- toDo.js
+++ toDo.js
@@ -1,17 +1,18 @@
+// Fetch HTML references
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const addBtn = document.getElementById("addButton");
 
-function addTask() {
-  const inputValue = inputBox.value;
-
-  if (inputValue !== "") {
-    let liElement = document.createElement("li");
-    liElement.innerText = inputValue;
-    listContainer.appendChild(liElement);
-  }
-}
-
-// for (const button of addButtonElements) {
 addBtn.addEventListener("click", addTask);
-// }
+
+// Fetch and display task input
+function addTask() {
+  // Creates list element, adds input content, displays list item in ul
+  const listItem = document.createElement("li");
+  listItem.innerText = inputBox.value;
+  listContainer.appendChild(listItem);
+
+  // Mark checkbox if task complete
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+}
