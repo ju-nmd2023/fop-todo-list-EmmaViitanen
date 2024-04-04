42bdcb2f83b90fbaea69130204952b5e52880eba
Emma Viitanen <viem23ni@student.ju.se>
2024-04-04 10:34:48 +0200

    Added som styling in css and start the delete button

--- toDo.js
+++ toDo.js
@@ -7,12 +7,32 @@ addBtn.addEventListener("click", addTask);
 
 // Fetch and display task input
 function addTask() {
-  // Creates list element, adds input content, displays list item in ul
-  const listItem = document.createElement("li");
-  listItem.innerText = inputBox.value;
-  listContainer.appendChild(listItem);
+  // If the input box is empty, show nothing
+  if (inputBox.value !== "") {
+    // Creates list element, adds input content, displays list item in ul
+    const listItem = document.createElement("li");
+    listItem.innerText = inputBox.value;
+    listContainer.appendChild(listItem);
 
-  // Mark checkbox if task complete
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+    // Create "completed" check box next to listItem
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function () {
+      toggleMarked(listItem, checkbox.checked);
+    });
+
+    listItem.append(checkbox);
+  }
+}
+
+// Mark checkbox if task complete
+function toggleMarked(listItem, isChecked) {
+  listItem.classList.toggle("marked", isChecked);
+}
+
+function deleteBtn() {
+  const deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click");
+  
+  if (d)
 }
